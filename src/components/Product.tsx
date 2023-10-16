13ed12064c008b493710ebd4d9b26b278e8bee50
vlladimirr21 <vlladimirr21@gmail.com>
2023-10-16 18:38:02 +0300

    first

---
+++ src/components/Product.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { IProduct } from '../models'
+
+interface ProductProps {
+  product: IProduct
+}
+
+export function Product(props: ProductProps) {
+  console.log(props)
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      {props.product.title}
+    </div>
+  )
+}
